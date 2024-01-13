2b3bd76b01c02a477ed33a8f124750326b88894b
Karim Altir <mhd.altir@student.ehb.be>
2024-01-13 16:35:33 +0100

    added Toolbar and The Fragment Container of list into MainActivity

--- app/src/main/java/be/ehb/valveandroid/MainActivity.java
+++ app/src/main/java/be/ehb/valveandroid/MainActivity.java
@@ -1,14 +1,43 @@
 package be.ehb.valveandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHost;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private NavController mNavController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        NavHostFragment mNavHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        mNavController = mNavHostFragment.getNavController();
+
+        // Specify the top-level destinations for the AppBarConfiguration
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.gamesFragment)
+                .build();
+
+        NavigationUI.setupActionBarWithNavController(this, mNavController, mAppBarConfiguration);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return mNavController.navigateUp() || super.onSupportNavigateUp();
     }
 }
