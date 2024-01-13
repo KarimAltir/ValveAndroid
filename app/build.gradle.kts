323e628ceb520cb45b687f203f1c4563976b0fae
Karim Altir <mhd.altir@student.ehb.be>
2024-01-13 16:55:31 +0100

    Added some few implementations for Images, RecycleView and for the navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,12 +31,18 @@ android {
 dependencies {
 
     implementation("androidx.room:room-common:2.6.0")
+    implementation("androidx.navigation:navigation-fragment:2.7.6")
+    implementation("androidx.navigation:navigation-ui:2.7.6")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.room:room-common:2.6.1")
     testImplementation("junit:junit:4.13.2")
